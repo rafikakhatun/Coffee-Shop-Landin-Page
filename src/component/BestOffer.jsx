babf15f3d59e6609dd9403e91fcbd6d04d865138
rafika khatun <rafikakhatun607@gmail.com>
2025-08-18 21:36:27 +0530

    feat: create BestOffer section

--- src/component/BestOffer.jsx
+++ src/component/BestOffer.jsx
@@ -4,22 +4,21 @@ import React from 'react';
 const BestOffer = () => {
     return (
         <div>
-            <section className='py-20 px-12 bg-gray-50'>
-                <div className='flex justify-between'>
-                    <div>
-                        <h1>That is Our Best Offer</h1>
-                        <p>A coffee shop will help you to tell the audience what your business</p>
+            <section className='py-20 px-12 bg-gray-100'>
+                <div className='flex flex-col md:flex-row justify-between p-8 space-y-6'>
+                    <div className='flex flex-col space-y-6'>
+                        <h1 className='text-2xl md:text-3xl  font-semibold text-center md:text-left '>That is Our Best Offer</h1>
+                        <p className='text-gray-700 text-center md:text-left'>A coffee shop will help you to tell the audience what your business</p>
                     </div>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 justify-center'>
                         <ChevronLeft className='w-10 h-10 bg-red-600 hover:bg-red-700 text-white rounded-full' />
                         <ChevronRight className='w-10 h-10 bg-red-600 hover:bg-red-700 text-white rounded-full' />
 
                     </div>
                 </div>
             </section>
-            
-        </div>
-    );
-};
 
+        </div>
+    )
+};
 export default BestOffer;
