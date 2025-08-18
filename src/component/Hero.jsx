e08cdbd82dcaf678c197b6687926e19e0d4aac82
rafika khatun <rafikakhatun607@gmail.com>
2025-08-18 21:00:44 +0530

    feat: complete responsive Hero section with navbar and mobile menu

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,11 @@
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, X } from "lucide-react";
 import heroBg from "../assets/herobg.jpeg";
+import { useState } from "react";
 
 function Hero() {
+
+  const [isOpen, setIsOpen] = useState(true)
+
   return (
     <section
       className="relative h-screen bg-cover bg-center"
@@ -13,6 +17,7 @@ function Hero() {
       {/* Navbar */}
       <nav className="relative z-10 flex justify-between items-center px-10 py-6 text-white">
         <h1 className="text-2xl md:text-3xl font-mono font-bold text-red-600">Coffee</h1>
+        {/*desktop menu */}
         <ul className="hidden md:flex gap-6 font-medium">
           <li className="hover:text-red-400 cursor-pointer">Home</li>
           <li className="hover:text-red-400 cursor-pointer">About Us</li>
@@ -20,8 +25,24 @@ function Hero() {
           <li className="hover:text-red-400 cursor-pointer">Review</li>
           <li className="hover:text-red-400 cursor-pointer">Contact</li>
         </ul>
-        <span className="flex md:hidden" ><AlignJustify className="w-8 h-8 hover:text-red-500" /></span>
+        <button onClick={() => setIsOpen(!isOpen)} className="flex md:hidden" >
+          {isOpen ? (<X className="w-8 h-8 hover:text-red-500" />): (<AlignJustify className="w-8 h-8 hover:text-red-500" />
+          ) }
+
+        </button>
       </nav>
+      {/*mobile menu */}
+      {isOpen && (<div className="">
+        <ul className="flex flex-col items-center justify-center md:hidden gap-6 absolute font-medium text-white w-full h-screen bg-black/70 z-20">
+          <li className="hover:text-red-400 cursor-pointer">Home</li>
+          <li className="hover:text-red-400 cursor-pointer">About Us</li>
+          <li className="hover:text-red-400 cursor-pointer">Menu</li>
+          <li className="hover:text-red-400 cursor-pointer">Review</li>
+          <li className="hover:text-red-400 cursor-pointer">Contact</li>
+        </ul>
+      </div>)}
+
+
 
       {/* Hero Content */}
       <div className="relative z-10 flex flex-col justify-center items-start h-full px-10 text-white">
