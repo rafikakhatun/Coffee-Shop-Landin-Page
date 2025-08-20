4b201630c032a3bbdbca34ee924c848ef96331a9
rafika khatun <rafikakhatun607@gmail.com>
2025-08-20 19:37:26 +0530

    install react icon

--- src/component/FollowUs.jsx
+++ src/component/FollowUs.jsx
@@ -4,7 +4,7 @@ const FollowUs = () => {
     return (
        <>
       <section className="w-full py-12">
-        <div style={{backgroundImage:`url(${bgFollow})`}}  className="w-full h-94 relative bg-no-repeat bg-center bg-cover">
+        <div style={{backgroundImage:`url(${bgFollow})`}}  className="w-full h-[450px] relative bg-no-repeat bg-center bg-cover">
             {/*overlay div */}
             <div className="absolute inset-0 bg-black/30"></div>
 
