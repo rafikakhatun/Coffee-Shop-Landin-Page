90d84a9e518ff3c0c4b31167f711965d71f9114d
rafika khatun <rafikakhatun607@gmail.com>
2025-08-21 15:06:58 +0530

    add header content in updatenews section

--- src/component/UpdateNews.jsx
+++ src/component/UpdateNews.jsx
@@ -5,9 +5,9 @@ const UpdateNews = () => {
         <>
         <section>
             {/*update header */}
-            <div>
-                <h1>Update News</h1>
-                <p>A cup of brewed cofee represents a contribute of up to 1.8 grams of <br/> fiber of the recommended.</p>
+            <div className='flex flex-col space-y-4 p-2'>
+                <h1 className='text-center text-2xl md:text-4xl font-extralight'>Update News</h1>
+                <p className='text-center text-gray-700 text-md md:text-xl'>A cup of brewed cofee represents a contribute of up to 1.8 grams of <br/> fiber of the recommended.</p>
             </div>
             {/*card section */}
             <section>
